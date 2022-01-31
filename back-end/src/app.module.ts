17dec0ff163c45c034df2a9454d8752042122b86
Yiğit Kaan Korkmaz <77935633+Evansmuscle@users.noreply.github.com>
2022-01-31 19:14:32 +0300

    Add groups resource

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -7,6 +7,7 @@ import { AppService } from './app.service';
 import { CustomConfigModule } from './configuration/configuration.module';
 import { CustomConfigService } from './configuration/configuration.service';
 import { UsersModule } from './users/users.module';
+import { GroupsModule } from './groups/groups.module';
 
 @Module({
   imports: [
@@ -16,6 +17,7 @@ import { UsersModule } from './users/users.module';
     }),
     UsersModule,
     CustomConfigModule,
+    GroupsModule,
   ],
   controllers: [AppController],
   providers: [AppService, CustomConfigService],
